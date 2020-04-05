91061206a733decda43735f9495d7e410d59f10c
izzipizzi <kircheiandri@gmail.com>
2020-04-05 10:20:48 +0300

    -

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -12,12 +12,28 @@ exports.create = (req,res)=>{
     form.parse(req,(err,fields,files)=>{
         if(err){
             return res.status(400).json({
-                err: 'Ця картинка не може бути загружена'+err
+                err: 'Ця картинка не може бути загружена'
             })
         }
-        let product = new Product(fields)
+        // check fields
+        const{name,description,price,category,quantity,shipping} = fields
+        if(!name || !description || !price || !category ||! quantity ||! shipping){
+            return res.status(400).json({
+                err: 'Всі поля мають бути заповнені'
+            })
+        }
 
+        let product = new Product(fields)
+        // 1kb =1000
+        //1 mb == 1000000
         if(files.photo){
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    err: 'Максимальний розмір картинки 1 мегабайт.'
+                })
+                
+            }
+            console.log('files_phot ',files.photo)
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
